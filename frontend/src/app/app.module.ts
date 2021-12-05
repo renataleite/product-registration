68e4b6eb869515ecfc4b935732c8a00017226c95
Renata <renataleitesg@gmail.com>
2021-12-05 21:56:22 +0000

    creates a button that navigates to the product creation page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
@@ -36,7 +37,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
